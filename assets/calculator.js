5cf4908f9a0e276223feec6b0e52766cf8228668
Mạnh <phanthemanh@hotmail.com>
2019-08-20 19:39:56 +0200

    Dark mode update
    Better effects and function with toggle class

--- assets/calculator.js
+++ assets/calculator.js
@@ -222,8 +222,24 @@ function play_sound() {
     bleep.play(); // Play button sound now
 }
 
-document.getElementById("dark_mode").onclick = function (){
-    document.body.style.backgroundColor="black";   
+// Dark mode
+let dark_mode = document.getElementById("dark_mode");
+let dark_mode_icon = document.createElement("img");
+dark_mode_icon.setAttribute("src", "assets/images/night.png");
+dark_mode.appendChild(dark_mode_icon);
+
+dark_mode_icon.onclick = function (){
+    document.body.classList.toggle("dark_body");
+    dark_mode.classList.toggle("night");
+
+    var is_night = dark_mode.classList.contains("night");
+    if (is_night) {
+        this.setAttribute("src", "assets/images/day.png");
+        this.style.backgroundColor = "#666666";
+    } else {
+        this.setAttribute("src", "assets/images/night.png");
+        this.style.backgroundColor = "#333333";
+    }
 };
 
 
